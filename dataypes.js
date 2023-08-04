9f01e02a1e4f5eae52a2ccab807063d4c5ca617a
idev009 <idev0091@gmail.com>
2023-08-04 14:05:05 +0530

    Starting with JS Basic , Variables, Data Type

---
+++ dataypes.js
@@ -0,0 +1,9 @@
+let firstName = "Ishan";
+let fatherName = "Laxman";
+let lastName = "Moorjmalani";
+
+let fullName = firstName + fatherName + lastName;
+
+console.log(firstName + " " + fatherName + " " + lastName);
+
+console.log(lastName);
