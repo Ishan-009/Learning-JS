2c38826ca4164d54a29597f1a33372b4dc8a51f6
Ishan-009 <ishanmoorjmalani009@gmail.com>
2023-08-09 12:54:23 +0530

    Conditinal Statements in JS

---
+++ conditionalStatement.js
@@ -0,0 +1,10 @@
+const numberOne = 10;
+const numberTwo = 20;
+
+if (numberOne > numberTwo) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+// with this we can control the structure of the code with conditionals which allows us to execute the code according to certain conditions
