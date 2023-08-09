5e0cabed7540f7a09cdd35042c1855a80cefb04f
Ishan-009 <ishanmoorjmalani009@gmail.com>
2023-08-09 10:43:48 +0530

    JS Basics Operators

--- dataypes.js
+++ dataypes.js
@@ -4,6 +4,16 @@ let lastName = "Moorjmalani";
 
 let fullName = firstName + fatherName + lastName;
 
-console.log(firstName + " " + fatherName + " " + lastName);
+let abc = undefined;
 
+let type = null;
+
+if (abc == undefined) {
+  type = true;
+}
+
+console.log(firstName + " " + fatherName + " " + lastName);
 console.log(lastName);
+console.log(abc);
+console.log(type);
+console.log(typeof lastName);
