8c5b108c3efd0a18c8420f34b519dbb1e1adee19
Ishan-009 <ishanmoorjmalani009@gmail.com>
2023-08-14 11:57:25 +0530

    Array Basic Methods

--- Basics-2/array.js
+++ Basics-2/array.js
@@ -16,3 +16,27 @@ console.log(Ishan);
 Ishan[2] = 2023 - 2001;
 
 console.log(age);
+
+// Array Methods
+
+const friends = ["Ram", "Lakhan", "Raju", "Raman"];
+
+console.log("New Element Added Index", friends.push("Chaman")); // push :- adds new element to the end of array and returns the index of it
+console.log(friends);
+
+console.log(friends.unshift("Raghu")); // adds element to the beginning of the array and returns new length of the array
+console.log(friends);
+
+// POP , shift
+
+console.log("pop", friends.pop()); // removes last element and returns the removed element
+console.log(friends);
+
+console.log("shift", friends.shift()); // removes first element and returns the removed element
+console.log(friends);
+
+// indexOf, and includes method
+
+console.log("indexOf", friends.indexOf("Ram")); // it returns the index of the searched element, adn if element noe present it returns -1;
+
+console.log(friends.includes("Ram")); // it returns true if value is present else false if value not present
