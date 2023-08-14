f49bcb4f270cd32fee5292faf356b20aa4133d37
Ishan-009 <ishanmoorjmalani009@gmail.com>
2023-08-14 19:07:14 +0530

    Loops:- ForLoops in JS

---
+++ Basics-2/loops.js
@@ -0,0 +1,35 @@
+// For Loop
+
+for (let i = 1; i <= 5; i++) {
+  console.log(`Excercise ${i}`);
+}
+
+const array = ["6351414924", "9824466500"];
+
+const newArray = []; // Empty Array
+for (let index = 0; index < array.length; index++) {
+  newArray[index] = array[index];
+}
+
+// Iteration over an array adn printing its value
+
+for (let arrIndex = 0; arrIndex <= array.length - 1; arrIndex++) {
+  console.log(`array[${arrIndex}] =>  `, array[arrIndex]);
+}
+
+// Looping Backwards through an array
+
+for (let arrIndex1 = array.length - 1; arrIndex1 >= 0; arrIndex1--) {
+  console.log(`array[${arrIndex1}] =>  `, array[arrIndex1]);
+}
+
+// looop inside loop
+
+for (let i = 1; i <= 5; i++) {
+  console.log(`Execercise - ${i}`);
+  for (let j = 1; j <= 5; j++) {
+    console.log(`-----Excercise Repetitation Set : ${j}`);
+  }
+}
+
+console.log("New Array content", newArray);
